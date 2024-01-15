c9f0edca900de76860589d83c1163e771952014e
Julian <julian@wheresnick.com.au>
2024-01-15 16:51:12 +1100

    fixed routes error

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
 import AboutMe from './sections/AboutMe/AboutMe';
